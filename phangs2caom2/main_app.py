9500229c92049ebd091cce09bb008d2fe7ad9222
Sharon Goliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2021-03-05 07:48:07 -0800

    PHANGS - interim commit.

--- phangs2caom2/main_app.py
+++ phangs2caom2/main_app.py
@@ -70,6 +70,20 @@
 """
 This module implements the ObsBlueprint mapping, as well as the workflow 
 entry point that executes the workflow.
+
+JJK - 05-10-20
+ALMA Telescopes: there are three telescopes that are part of the ALMA
+observatory:
+Main Array: 50 x 12m antennas
+Total Power Array: 4 x 12m antennas
+Atacama Compact Array (ACA): 12 x 7m antennas
+
+CW - 20-10-20
+In CAOM I would have telescope be the telescope and instrument the instrument
+on the telescope. How results get shown is another matter. We should add
+telescope to the list of returned fields, then it can be set to be in the
+default set for PHANGS and hidden for others.
+
 """
 
 import importlib
@@ -81,6 +95,8 @@ import traceback
 from math import sqrt
 
 from caom2 import Observation, DataProductType, CalibrationLevel
+from caom2 import CoordBounds1D, RefCoord, CoordAxis1D, Provenance
+from caom2 import CoordRange1D, Axis, TemporalWCS, Proposal, ProductType
 from caom2utils import ObsBlueprint, get_gen_proc_arg_parser, gen_proc
 from caom2pipe import manage_composable as mc
 
@@ -102,34 +118,89 @@ class PHANGSName(mc.StorageName):
 
     PHANGS_NAME_PATTERN = '*'
 
-    def __init__(self, obs_id=None, file_name=None, entry=None,
-                 artifact_uri=None):
+    def __init__(self, file_name=None, artifact_uri=None, entry=None):
         if file_name:
             self.fname_in_ad = file_name
+            self._file_name = file_name
+            self._file_id = mc.StorageName.remove_extensions(file_name)
+            self._assign_bits()
             super(PHANGSName, self).__init__(
-                obs_id, COLLECTION, PHANGSName.PHANGS_NAME_PATTERN,
+                self._obs_id, COLLECTION, PHANGSName.PHANGS_NAME_PATTERN,
                 fname_on_disk=file_name, compression='', entry=entry)
         elif artifact_uri:
             scheme, path, file_name = mc.decompose_uri(artifact_uri)
+            self._file_name = file_name
+            self._file_id = mc.StorageName.remove_extensions(file_name)
+            self._assign_bits()
         super(PHANGSName, self).__init__(
-            obs_id, COLLECTION, PHANGSName.PHANGS_NAME_PATTERN,
+            self._obs_id, COLLECTION, PHANGSName.PHANGS_NAME_PATTERN,
             fname_on_disk=file_name, compression='', entry=entry)
-        self._file_id = mc.StorageName.remove_extensions(file_name)
-        self._obs_id = self.file_id
+
+    def __str__(self):
+        return f'obs_id {self._obs_id}, ' \
+               f'file_id {self._file_id}'
+
+    @property
+    def algorithm_name(self):
+        return self._algorithm_name
+
+    @property
+    def energy_transition(self):
+        return self._energy_transition
 
     @property
     def file_id(self):
         return self._file_id
 
     @property
+    def file_name(self):
+        return self._file_name
+
+    @property
     def product_id(self):
-        result = 'spectral_line_data_cube'
-        if 'derived' in self.file_name:
-            result = 'image_intensity_map'
-        return result
+        return self._product_id
+
+    @property
+    def target_name(self):
+        return self._target_name
+
+    @property
+    def telescope(self):
+        return self._telescope
+
+    def is_derived(self):
+        return 'mom' in self._file_id
 
     def is_valid(self):
         return True
+
+    def _assign_bits(self):
+        # the right-hand side of the dictionary comes from ALMA/ALMACA
+        # collections telescope names
+        telescope_lookup = {
+            '7m+tp': 'ALMA-7m + ALMA-TP',
+            '7m+12m': 'ALMA-7m + ALMA-12m',
+            '12m+7m': 'ALMA-7m + ALMA-12m',
+            'tp+12m': 'ALMA-12m + ALMA-TP',
+            '12m+tp': 'ALMA-12m + ALMA-TP',
+            'tp+7m': 'ALMA-7m + ALMA-TP',
+            '12m+7m+tp': 'ALMA-12m + ALMA-7m + ALMA-TP',
+        }
+        bits = self._file_id.split('_')
+        self._obs_id = f'{bits[0]}_{bits[1]}_{bits[2]}'
+        self._algorithm_name = f'{bits[2]}_datacube'
+        if len(bits) >= 4:
+            self._algorithm_name = ''.join(ii for ii in bits[3:])
+        self._energy_transition = bits[2]
+        self._target_name = bits[0]
+        self._telescope = telescope_lookup.get(bits[1])
+
+        # ER - original emails
+        self._product_id = self._file_id
+
+        if self._telescope is None:
+            raise mc.CadcException(
+                f'Unexpected telescope value in {self._file_id}')
 
 
 def accumulate_bp(bp, uri):
@@ -138,19 +209,39 @@ def accumulate_bp(bp, uri):
     logging.debug('Begin accumulate_bp.')
     bp.configure_position_axes((1, 2))
     bp.configure_energy_axis(3)
+    bp.configure_observable_axis(4)
 
-    bp.clear('Observation.telescope.name')
-    bp.add_fits_attribute('Observation.telescope.name', 'TELESCOP')
+    storage_name = PHANGSName(artifact_uri=uri)
+
+    # all DerivedObservations, even though members are unknown at CADC
+    bp.set('DerivedObservation.members', {})
+    bp.set('Observation.algorithm.name', storage_name.algorithm_name)
+
+    if 'ALMA' in storage_name.telescope:
+        # ER 15-10-20
+        # For ALMA, the 'filter' field could be set to Band6, which is the
+        # receiver band used by the observations and thus forms a useful
+        # analogue to the "Filter" set for optical telescopes.
+        # CW 20-10-10
+        # In CAOM I would have telescope be the telescope and instrument the
+        # instrument on the telescope.
+        # SGo - so, use the ALMA/ALMACA instrument names accordingly
+        bp.set('Observation.instrument.name', 'Band 6')
+
+    bp.set('Observation.target.name', storage_name.target_name)
+
+    bp.set('Observation.telescope.name', storage_name.telescope)
     bp.set('Observation.telescope.geoLocationX', 2225015.30883296)
     bp.set('Observation.telescope.geoLocationY', -5440016.41799762)
     bp.set('Observation.telescope.geoLocationZ', -2481631.27428014)
 
     data_product_type = DataProductType.CUBE
     calibration_level = CalibrationLevel.PRODUCT
-    if 'derived' in uri:
-        # I'm sure this is wrong
-        data_product_type = DataProductType.MEASUREMENTS
-        calibration_level = CalibrationLevel.ANALYSIS_PRODUCT
+    if 'mom' in uri:
+        # ER 4-10-20
+        # IMAGE -- These are 2D image-like maps of the galaxy so are most
+        # IMAGE like.
+        data_product_type = DataProductType.IMAGE
 
     bp.set('Plane.calibrationLevel', calibration_level)
     bp.set('Plane.dataProductType', data_product_type)
@@ -159,13 +250,21 @@ def accumulate_bp(bp, uri):
     bp.clear('Plane.metaRelease')
     bp.add_fits_attribute('Plane.metaRelease', 'DATE')
 
-    bp.clear('Plane.provenance.name')
-    bp.add_fits_attribute('Plane.provenance.name', 'ORIGIN')
-    bp.clear('Plane.provenance.lastExecuted')
-    bp.add_fits_attribute('Plane.provenance.lastExecuted', 'DATE')
-    bp.clear('Plane.provenance.producer')
+    artifact_product_type = ProductType.SCIENCE
+    if 'noise' in uri:
+        artifact_product_type = ProductType.NOISE
+    elif 'mask' in uri:
+        artifact_product_type = ProductType.CALIBRATION
+    bp.set('Artifact.productType', artifact_product_type)
+
+    # bp.clear('Plane.provenance.name')
+    # bp.add_fits_attribute('Plane.provenance.name', 'ORIGIN')
+    # bp.clear('Plane.provenance.lastExecuted')
+    # bp.add_fits_attribute('Plane.provenance.lastExecuted', 'DATE')
+    # bp.clear('Plane.provenance.producer')
 
     # chunk level
+    # position
     bp.clear('Chunk.position.axis.function.cd11')
     bp.clear('Chunk.position.axis.function.cd22')
     bp.add_fits_attribute('Chunk.position.axis.function.cd11', 'CDELT1')
@@ -174,6 +273,16 @@ def accumulate_bp(bp, uri):
     bp.add_fits_attribute('Chunk.position.axis.function.cd22', 'CDELT2')
     bp.set('Chunk.position.resolution', '_get_position_resolution(header)')
 
+    # energy
+    bp.set('Chunk.energy.transition.species', storage_name.energy_transition)
+    bp.set('Chunk.energy.transition.transition', 'TBD')
+
+    # observable
+    bp.clear('Chunk.observable.dependent.axis.ctype')
+    bp.add_fits_attribute('Chunk.observable.dependent.axis.ctype', 'BTYPE')
+    bp.clear('Chunk.observable.dependent.axis.cunit')
+    bp.add_fits_attribute('Chunk.observable.dependent.axis.cunit', 'BUNIT')
+    bp.set('Chunk.observable.dependent.bin', 1)
     logging.debug('Done accumulate_bp.')
 
 
@@ -198,6 +307,8 @@ def update(observation, **kwargs):
     if phangs_name is None:
         raise mc.CadcException(f'Need one of fqn or uri defined for '
                                f'{observation.observation_id}')
+
+    _update_from_comment(observation, phangs_name, headers)
 
     logging.debug('Done update.')
     return observation
@@ -229,23 +340,99 @@ def _get_position_resolution(header):
     # From
     # https://open-confluence.nrao.edu/pages/viewpage.action?pageId=13697486
     # Clare Chandler via JJK - 21-08-18
-    return 3600.0 * sqrt(bmaj*bmin)
+    result = None
+    if bmaj is not None and bmin is not None:
+        result = 3600.0 * sqrt(bmaj*bmin)
+    return result
 
 
 def _get_uris(args):
     result = []
-    if args.local:
+    if args.lineage:
+        for ii in args.lineage:
+            result.append(ii.split('/', 1)[1])
+    elif args.local:
         for ii in args.local:
             file_id = mc.StorageName.remove_extensions(os.path.basename(ii))
             file_name = f'{file_id}.fits'
             result.append(PHANGSName(file_name=file_name).file_uri)
-    elif args.lineage:
-        for ii in args.lineage:
-            result.append(ii.split('/', 1)[1])
     else:
         raise mc.CadcException(
             f'Could not define uri from these args {args}')
     return result
+
+
+def _update_from_comment(observation, phangs_name, headers):
+    # From ER: 04-03-21
+    # COMMENT Produced with PHANGS-ALMA pipeline version 4.0 Build 935
+    # - Provenance.version
+    # COMMENT Galaxy properties from PHANGS sample table version 1.6
+    # COMMENT Calibration Level 4 (ANALYSIS_PRODUCT)
+    # - Calibration level (either 3 or 4)
+    # COMMENT PHANGS-ALMA Public Release 1
+    # - Provenance.project = PHANGS-ALMA
+    # COMMENT Generated by the Physics at High Angular resolution
+    # COMMENT in nearby GalaxieS (PHANGS) collaboration
+    # - Provenance.organization = PHANGS
+    # COMMENT Canonical Reference: Leroy et al. (2021), ApJ, Submitted
+    # - Update to reference when accepted
+    # COMMENT Release generated at 2021-03-04T07:28:10.245340
+    # - Provenance.lastExecuted
+    # COMMENT Data from ALMA Proposal ID 2017.1.00886.L
+    # - Proposal.proposalID
+    # COMMENT Observed in MJD interval [58077.386275,58081.464121]
+    # COMMENT Observed in MJD interval [58290.770032,58365.629222]
+    # COMMENT Observed in MJD interval [58037.515807,58047.541173]
+    # COMMENT Observed in MJD interval [58353.589805,58381.654757]
+    # COMMENT Observed in MJD interval [58064.3677,58072.458597]
+    # COMMENT Observed in MJD interval [58114.347649,58139.301879]
+    chunk = None
+    for plane in observation.planes.values():
+        if plane.product_id != phangs_name.product_id:
+            continue
+        if plane.provenance is None:
+            plane.provenance = Provenance(name='PHANGS-ALMA pipeline')
+
+        for artifact in plane.artifacts.values():
+            if artifact.uri != phangs_name.file_uri:
+                continue
+            for part in artifact.parts.values():
+                chunk = part.chunks[0]
+                break
+
+        for entry in headers[0].get('COMMENT'):
+            if 'pipeline version ' in entry:
+                plane.provenance.version = entry.split(' version ')[1]
+            elif 'Calibration Level' in entry:
+                level = entry.split()[2]
+                if level == '4':
+                    plane.calibration_level = CalibrationLevel.ANALYSIS_PRODUCT
+            elif 'PHANGS-ALMA Public Release' in entry:
+                plane.provenance.project = 'PHANGS-ALMA'
+            elif 'in nearby GalaxieS (PHANGS) collaboration' in entry:
+                plane.provenance.organization = 'PHANGS'
+            elif 'Release generated at ' in entry:
+                plane.provenance.last_executed = mc.make_time_tz(
+                    entry.split(' at ')[1])
+            elif 'Data from ALMA ProposalID ' in entry:
+                if observation.proposal is None:
+                    observation.proposal = Proposal(entry.split(' ID ')[1])
+            # elif 'Canonical Reference: ' in entry:
+            #     plane.provenance.reference = entry.split(': ')[1]
+            elif 'Observed in MJD interval ' in entry:
+                if chunk is not None:
+                    bits = entry.split()[4].split(',')
+                    start_ref_coord = RefCoord(
+                        0.5, mc.to_float(bits[0].replace('[', '')))
+                    end_ref_coord = RefCoord(
+                        1.5, mc.to_float(bits[1].replace(']', '')))
+                    sample = CoordRange1D(start_ref_coord, end_ref_coord)
+                    if chunk.time is None:
+                        coord_bounds = CoordBounds1D()
+                        axis = CoordAxis1D(axis=Axis('TIME', 'd'))
+                        chunk.time = TemporalWCS(axis, timesys='UTC')
+                        chunk.time.axis.bounds = coord_bounds
+                    chunk.time.axis.bounds.samples.append(sample)
 
 
 def to_caom2():
