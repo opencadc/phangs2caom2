6b31da8f847ef2c9bd88f9f0529dd415f9c85717
Sharon Goliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2021-03-23 10:45:35 -0700

    CADC-8516 - named the main file composed, not composable.

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -5,16 +5,4 @@ then
   cp /usr/local/bin/config.yml ${PWD}
 fi
 
-if [[ ! -e ${PWD}/state.yml ]]; then
-  if [[ "${@}" == "phangs_run_state" ]]; then
-    yesterday=$(date -d yesterday "+%d-%b-%Y %H:%M")
-    echo "bookmarks:
-    phangs_timestamp:
-      last_record: $yesterday
-" > ${PWD}/state.yml
-  else
-    cp /usr/local/bin/state.yml ${PWD}
-  fi
-fi
-
 exec "${@}"
